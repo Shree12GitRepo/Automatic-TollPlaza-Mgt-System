a982b137af258be6c6d9eadc44ea0156b8a304d4
Shree12GitRepo <sahooshree1@gmail.com>
2019-10-22 23:22:13 +0530

    Spring Boot RTO-Registration Distributed Application directory Structure

---
+++ RTO-Registration/src/main/java/com/USA/RTO/RtoRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.USA.RTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtoRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtoRegistrationApplication.class, args);
+	}
+
+}
---
+++ RTO-Registration/src/test/java/com/USA/RTO/RtoRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.USA.RTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtoRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
